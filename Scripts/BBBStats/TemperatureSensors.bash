8d6b15d494642ef2c30ccb70c4fcee72fb58a59d
michaelvaes <michael.vaes@me.com>
2015-12-06 12:22:48 +0100

    Noob bash bugfix

--- Scripts/BBBStats/TemperatureSensors.bash
+++ Scripts/BBBStats/TemperatureSensors.bash
@@ -2,12 +2,12 @@
 declare -A SENSORS;
 
 # BBBOFFICE
-if [ "$(hostname)" -eq "bbboffice.michaelvaes.be" ]; then
+if [ "$(hostname)" = "bbboffice.michaelvaes.be" ]; then
 	API_KEY="8DLTGR4JMAZ7SGO1";	
 	SENSORS["28-0000057ee007"]="&Office&field1";
 
 # BBBGARDEN
-elif [ "$(hostname)" -eq "bbbgarden.michaelvaes.be" ]; then
+elif [ "$(hostname)" = "bbbgarden.michaelvaes.be" ]; then
 	API_KEY="7ZC64ODGH4PHM7MW";
 	SENSORS["28-0000057ee219"]="&Garden&field1"; # Inside
 	SENSORS["28-0014136d7bff"]="&Garden&field2"; # Outside
